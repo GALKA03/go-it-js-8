b5aca4379fe7ba729b2776e0ee9797a7f433c17a
Galina Matvienko <galyagmgm@gmail.com>
2022-09-23 12:25:31 +0200

    commit
    finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,34 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import  SimpleLightbox  from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+
+// Change code below this line
+const imgConteiner = document.querySelector('.gallery');
+const galleryMarcup = createGalaryMarcup(galleryItems);
+imgConteiner.insertAdjacentHTML('beforeend',galleryMarcup)
+let lightbox = new SimpleLightbox('.gallery a', {captionDelay:250 });
+    console.log(lightbox)
+
+imgConteiner.addEventListener('click', onPicturesClick)
+
+function createGalaryMarcup(galleryItems){
+    return galleryItems
+    .map(({ preview, original, description }) => {
+      return` 
+      <a class="gallery__item" href="${ original}">
+  <img class="gallery__image"src="${preview}"alt="${description}" title= ${description}/>
+</a>`}).join('');
+}
+
+function onPicturesClick(event){
+  event.preventDefault();
+
+ const noEventGallary = event.target.nodeName;
+     //если действие происходит не в нашем диве == ничего не происходит 
+  if (noEventGallary !== 'IMG'){
+        return;
+    }
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
 
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const SECONDS = 'videoplayer-current-time'
+  
+        const iframe = document.querySelector('iframe');
+        const player = new Player(iframe);
+
+    player.on('timeupdate',throttle(onPlayer, 1000));
+   
+function onPlayer({seconds}) {
+
+    localStorage.setItem(SECONDS, seconds) 
+
+}
+ 
+ player.setCurrentTime(localStorage.getItem(SECONDS)).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
+
+
