d3284a9b9a40398b0d01c23397e0c3694c0dbc57
Galina Matvienko <galyagmgm@gmail.com>
2022-09-24 15:48:38 +0200

    lll

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,15 +9,9 @@ const message = document.querySelector('.feedback-form textarea')
 const email = document.querySelector('.feedback-form input')
 form.addEventListener('input', throttle(onDataForm, 500));
 
-
-
-
 const formData = {};
-//dataFromLocalStorage();
-//console.log('email:',email)
+
 form.addEventListener('submit', onFormSubmit);
-//message.addEventListener('input',throttle(onTexteriaInput, 500));
-//email.addEventListener('input', throttle(onEmailInput, 500));
 
 
 function onDataForm(e) {
@@ -25,12 +19,18 @@ function onDataForm(e) {
     formData[e.target.name] = e.target.value;
     console.log(formData)
 localStorage.setItem(MESSAGE, JSON.stringify(formData))
-   
-
 }
 
+(function populateMassageTexteria() {
+const savedMassage = JSON.parse(localStorage.getItem(MESSAGE));
+//localStorage.removeItem(MESSAGE)
+    if (savedMassage) {
+        message.value = savedMassage
+     email.value = savedMassage.email;
+  message.value = savedMassage.message;
+    }
+})()
 
-//populateMassageTexteria();
 function onFormSubmit(e) {
  e.preventDefault();
 
@@ -44,24 +44,3 @@ function onFormSubmit(e) {
 }
 
 
-// function onTexteriaInput(e){
-//     const valueMessege = e.target.value;
-//     console.log(valueMessege)
-// //     localStorage.setItem(MESSAGE, valueMessege)
-//  }
-
-// function onEmailInput(e) {
-//     const valueEmail = e.target.value;
-//     console.log(valueEmail)
-// //     localStorage.setItem(EMAIL, valueEmail)
-//  }
-
-(function populateMassageTexteria() {
-const savedMassage = JSON.parse(localStorage.getItem(MESSAGE));
-//localStorage.removeItem(MESSAGE)
-    if (savedMassage) {
-        message.value = savedMassage
-     email.value = savedMassage.email;
-  message.value = savedMassage.message;
-    }
-})()
