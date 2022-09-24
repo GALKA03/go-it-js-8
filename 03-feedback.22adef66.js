75bc2ec6ca325c223aacb2a78a90fb9e229f88ac
GALKA03 <102662350+GALKA03@users.noreply.github.com>
2022-09-24 08:29:51 +0000

    Deploying to gh-pages from @ GALKA03/go-it-js-8@87cee8484883ce5df8e88db1837d3cf2eff09993 🚀

---
+++ 03-feedback.22adef66.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");console.log("form:",e);document.querySelector(".feedback-form textarea"),document.querySelector(".feedback-form input");
+//# sourceMappingURL=03-feedback.22adef66.js.map
---
+++ 03-feedback.944e7229.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");console.log("form:",e);document.querySelector(".feedback-form textarea"),document.querySelector(".feedback-form input")}();
+//# sourceMappingURL=03-feedback.944e7229.js.map
