4637fde1ca148ca663298509461e157d88593847
GALKA03 <102662350+GALKA03@users.noreply.github.com>
2022-09-24 13:26:15 +0000

    Deploying to gh-pages from @ GALKA03/go-it-js-8@8767476d4b95d9de12fb5ec3df8ca75308ba1f0a 🚀

--- 03-feedback.944e7229.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".feedback-form");console.log("form:",e);document.querySelector(".feedback-form textarea"),document.querySelector(".feedback-form input")}();
-//# sourceMappingURL=03-feedback.944e7229.js.map
